019d622ccc39db404d87deb408e75d4df2b85687
sofiet <sofiet@stud.ntnu.no>
2020-02-19 11:42:58 +0100

    Exercise 5

--- driver-go/main.go
+++ driver-go/main.go
@@ -8,27 +8,36 @@ func main(){
     numFloors := 4
 
     elevio.Init("localhost:15657", numFloors)
-    
+
     var d elevio.MotorDirection = elevio.MD_Up
     //elevio.SetMotorDirection(d)
-    
+
     drv_buttons := make(chan elevio.ButtonEvent)
     drv_floors  := make(chan int)
     drv_obstr   := make(chan bool)
-    drv_stop    := make(chan bool)    
-    
+    drv_stop    := make(chan bool)
+
     go elevio.PollButtons(drv_buttons)
     go elevio.PollFloorSensor(drv_floors)
     go elevio.PollObstructionSwitch(drv_obstr)
     go elevio.PollStopButton(drv_stop)
-    
-    
+
+
     for {
         select {
         case a := <- drv_buttons:
             fmt.Printf("%+v\n", a)
             elevio.SetButtonLamp(a.Button, a.Floor, true)
-            
+            //Endringer
+            b := <- drv_floors
+            if a.Floor < b {
+              elevio.SetMotorDirection(elevio.MD_Down)
+            }else if a.Floor >  b {
+              elevio.SetMotorDirection(elevio.MD_Up)
+            }else{
+              elevio.SetMotorDirection(elevio.MD_Stop)
+            }
+
         case a := <- drv_floors:
             fmt.Printf("%+v\n", a)
             if a == numFloors-1 {
@@ -37,8 +46,12 @@ func main(){
                 d = elevio.MD_Up
             }
             elevio.SetMotorDirection(d)
-            
-            
+            //Endringer
+            elevio.SetFloorIndicator(a)
+            /*if ((elevio.getButton(elevio.BT_HallUP, a) && d == elevio.MD_Up) || (elevio.getButton(elevio.BT_HallDown, a) && d == elevio.MD_Down) || elevio.getButton(elevio.BT_Cab, a)) {
+              d = elevio.MD_Stop
+            }*/
+
         case a := <- drv_obstr:
             fmt.Printf("%+v\n", a)
             if a {
@@ -46,7 +59,7 @@ func main(){
             } else {
                 elevio.SetMotorDirection(d)
             }
-            
+
         case a := <- drv_stop:
             fmt.Printf("%+v\n", a)
             for f := 0; f < numFloors; f++ {
@@ -55,5 +68,5 @@ func main(){
                 }
             }
         }
-    }    
+    }
 }
