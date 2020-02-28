a02f646a3c79b31234875cc1a26323688e90debd
Siri Hegsvold <sirihhe@stud.ntnu.no>
2020-02-28 15:40:37 +0100

    started to implement timer

---
+++ project/timer.go
@@ -0,0 +1,47 @@
+
+import
+  "time"
+
+
+var timerFlag int
+
+func timer_start(){
+  timerFlag = 1
+}
+
+func MotorTimer(finished chan<- bool, start <-chan bool, motorMotionTimer time.Duration)
+
+  motorTimer := time.NewTimer(4 * time.Second)
+
+
+
+  for {
+    select {
+    case <-start:
+      motorTimer.Reset(motorMotionTimer)
+    case <-motorTimer.:
+      finished <- true
+    }
+  }
+}
+
+
+func DoorTimer(finished chan<- bool, start <-chan bool, doorOpenTime time.Duration) {
+
+	doorTimer := time.NewTimer(3 * time.Second)
+
+	if !doorTimer.Stop() {
+		<-doorTimer.C
+	}
+
+	for {
+		select {
+		case <-start:
+			doorTimer.Reset(doorOpenTime)
+		case <-doorTimer.:
+			finished <- true
+		}
+	}
+}
+
+motorTimedOut.Stop()
