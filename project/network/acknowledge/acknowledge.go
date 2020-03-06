37e6be80e6d6efa87fdb6aa770b0799cd1e4c834
Siri Hegsvold <sirihhe@stud.ntnu.no>
2020-03-06 15:03:37 +0100

    some changes to ack

--- project/network/acknowledge/acknowledge.go
+++ project/network/acknowledge/acknowledge.go
@@ -7,4 +7,27 @@ import (
    "time"
    "os"
    "sync"
+   //+++
 )
+
+type AckMessage struct {
+  node_id string
+  timestamp string
+  error_id string
+  state int
+  order_list string
+  confirmed_orders string
+  current_orders string
+}
+
+type SentMessage struct {
+  UpdateMessage map[int]
+  StatusMessage map[int]
+  NumberOfSentMessage map[int]int
+  NotRecFromSlave map[int][]string
+}
+
+type AckStruct struct {
+  AckMsg AckMessage
+  AckTimer *time.Timer
+}
--- project/timer/timer.go
+++ project/timer/timer.go
@@ -1,39 +1,24 @@
 package timer
 
-/*
-import(
+//funksjons -& variablenavn må oppdateres etter hva som blir benyttet i de andre modulene
+
+import
   "time"
-)
-
-var timerFlag int
-
-func timer_start(){
-  timerFlag = 1
-}
-
-func MotorTimer(finished chan<- bool, start <-chan bool, motorMotionTimer time.Duration)
-
-  motorTimer := time.NewTimer(4 * time.Second)
 
 
+//var timerFlag int
 
-  for {
-    select {
-    case <-start:
-      motorTimer.Reset(motorMotionTimer)
-    case <-motorTimer.:
-      finished <- true
-    }
-  }
-}
-
+//func timer_start(){
+//  timerFlag = 1
+//}
 
 func DoorTimer(finished chan<- bool, start <-chan bool, doorOpenTime time.Duration) {
 
 	doorTimer := time.NewTimer(3 * time.Second)
 
+  //empty the channel -> not concurrent receivers
 	if !doorTimer.Stop() {
-		<-doorTimer.C
+		<-doorTimer
 	}
 
 	for {
@@ -46,7 +31,59 @@ func DoorTimer(finished chan<- bool, start <-chan bool, doorOpenTime time.Durati
 	}
 }
 
-motorTimedOut.Stop()
 
 
-*/
+func hasOrders(globalState GlobalElevator) bool {
+	for f := range globalState.HallRequests { //floor
+		for b := range globalState.HallRequests[f] { //button
+			if globalState.HallRequests[f][b] {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+
+func MotorTimer(timeout chan<- bool, globalState <-chan GlobalElevator, motorMotionTimer time.Duration) {
+
+    floorMap := make(map[string]int)
+    motorTimerEnabled:= false
+  	motorTimer := time.NewTimer(timeout)
+
+  	for {
+  		select {
+  		case newGlobalState := <-globalState:
+  			//motortimer is enabled when there exists hall orders
+  			motorTimerEnabled = hasOrders(newGlobalState)
+
+  			// Reset timer if an elevator has reached a new floor
+  			for newElevID, newElev := range newGlobalState.Elevators {
+  				if floor, ok := floorMap[newElevID]; ok {
+  					if floor != newElev.Floor {
+  						if motorTimer.Stop() {
+  							motorTimer.Reset(timeout)
+  						}
+  					}
+  				}
+  				floorMap[newElevID] = newElev.Floor
+  			}
+
+  	//motortimer timed out
+    case <- motorTimer:
+  			timeout <- true
+  			mototTimer.Reset(timeout)
+
+  		default:
+  			if !motorTimerEnabled && motorTimer.Stop() {
+  				motorTimer.Reset(timeout)
+  			}
+  		}
+  	}
+  }
+
+// Variabler mest sannsynlig nødvendig i FSM:
+  //doorTimerFinished <-chan bool,
+	//doorTimerStart chan<- bool,
+  //motorTimeout <-chan bool,
+	//motorUpdateState chan<- GlobalElevator
