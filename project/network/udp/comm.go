3a42fbee296bfae5723761af76d46de252f6eed0
karenskn <karen@aknutsen.org>
2020-03-11 09:24:44 +0100

    ..

--- project/network/udp/comm.go
+++ project/network/udp/comm.go
@@ -1,6 +1,7 @@
 package comm
 
 import(
+  "time"
   "net"
   "fmt"
   "./udp"
@@ -13,26 +14,26 @@ const wPort := 5678
 func messaging(){
   reciever, sender := udp.Init(rPort, wPort)
   timeOut := make(chan bool) //ha en måte å se at det har tatt for lang tid før man har fått svar
-  go senderTimeOut(timOut, sender)
+  go sendTimeOut(timOut, sender)
+  //Must finish
 }
 
 
-func senderTimeOut(timeOut chan bool, sender chan Packet){
-  var packet Packet
+//Function that lets a packet be sent up to ten times if no ack is recived
+func sendTimeOut(timeOut chan bool, sender chan Packet, reciever chan Packet){
+  var senderPacket Packet
+  var reciverPacket Packet
   switch expression {
-  case sender<-packet:
-    if packet.packetID < 10{
-      packet.packetID++
-      packet->sender
+  senderPacket->sender
+  case senderPacket.message_nr < 10:
+    if reciever<-recieverPacket & senderPacket.ID == recieverPacket.ID { //Funker dette??
+      time.Sleep(0.05 * time.Second)
+      senderPacket.message_nr++
+      senderPacket->sender
     }else{
       true->timeout
     }
-  case timeOut<-false:
-    fmt.Println("Timeout. Packet used too long to send.")
+  case timeOut<-true:
+    fmt.Println("Timeout. Packet used too long time to be sent.")
   }
-}
-
-func ackTimeOut(timeOut chan bool, ack chan Packet){
-  var packet Packet
-  
 }
